94ef98f1a5170c052f5cd83296ab26b225b65764
dahyunkim <dahyun_ee@naver.com>
2023-01-27 06:07:32 +0900

    [Rename] image 폴더 [Fix

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import router from './domain';
 import cors from "cors";
 import { corsMiddleware, globalErrorHandler } from './global/middlewares';
+import path from 'path';
 
 const app = express(); 
 const PORT = 3000;
@@ -23,7 +24,7 @@ app.use(corsMiddleware(corsOriginList));
 app.use(express.json());
 app.use("/", router); 
 app.use(globalErrorHandler);
-
+app.use(express.static(path.join(__dirname, 'static')));
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.send("BTS 손흥민 봉준호 Track-1 Let's go!!!🔥🔥🔥");
 });
