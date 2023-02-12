5de20ed656425f7e5ff005e213c2f774b6f9c8ee
dahyunkim <dahyun_ee@naver.com>
2023-02-13 00:12:00 +0900

    [Style] 코드 포맷 변경

--- src/global/modules/S3Object/delete/deleteOneBeat.ts
+++ src/global/modules/S3Object/delete/deleteOneBeat.ts
@@ -3,11 +3,6 @@ import multipartS3 from '../../../../infra/aws/s3MultipartConfig';
 import config from '../../../config';
 import { rm } from '../../../constants';
 
-const deleteParams = {
-    Bucket: config.tracksBucketName,
-    Delete: { Objects: [] },
-}
-
 //! S3 버킷에서 게시글의 오디오파일객체, 자켓이미지객체 삭제하기 
 const deleteS3AudioAndImage = async (audioFile: string, imageFile: string) => {
     try {
@@ -19,12 +14,13 @@ const deleteS3AudioAndImage = async (audioFile: string, imageFile: string) => {
             return { Key: obj.value }
         });
 
-        const objectParams = {
-            Bucket: config.tracksBucketName,
-            Delete: { Objects: objects }
-        };
-
-        await multipartS3.deleteObjects(objectParams).promise();
+        await multipartS3
+                .deleteObjects
+                    ({
+                        Bucket: config.tracksBucketName,
+                        Delete: { Objects: objects }
+                    })
+                .promise();
     } catch (error) {
         throw new DeleteTrackS3Object(rm.FAIL_DELETE_S3_TRACK_AUDIO_AND_IMAGE_OBJECT);
     }
