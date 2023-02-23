580cf829d70e90ebd246880301b7830c09ea2a99
dahyunkim <dahyun_ee@naver.com>
2023-02-24 04:12:13 +0900

    [Feature] /profile/producer/:producerId/beats?page=&limit= GET 프로듀서 보컬서칭 조회 기능 추가

---
+++ src/domain/profile/repository/findProducerBeatsById.ts
@@ -0,0 +1,56 @@
+import { InvalidProducer } from './../../../global/middlewares/error/errorInstance';
+import config from '../../../global/config';
+import prisma from '../../../global/config/prismaClient';
+import { BeatReturnDTO, PortfolioDTO, ProducerProfileDTO } from '../interfaces';
+import { rm } from '../../../global/constants';
+import { getS3OneBeatObject, getS3OneImageObject } from '../../../global/modules/S3Object/get';
+import { producerTitleAsPortfolioDTO } from '.';
+
+function objectParams_url(bucketName: string, fileKey: string) {
+    return {
+        Bucket: bucketName,
+        Key: fileKey,
+    };
+};
+
+const findProducerBeatsById = async(producerId: number, limit: number, page: number) => {
+    try {
+        const data = await prisma.beat
+            .findMany({
+                where: { producerId },
+                orderBy: { createdAt: 'desc' },
+                skip: (page-1)*limit,
+                take: limit,
+            })
+            .then(async (beat) => {
+                const beatList = await Promise.all(beat.map(async (beat) => {
+                    const beatURL = await getS3OneBeatObject(objectParams_url(config.tracksBucketName, beat.beatFile));
+                    const imageURL = (beat.beatImage === config.defaultJacketAndProducerPortfolioImage) ? 
+                                        beat.beatImage : 
+                                        await getS3OneImageObject(objectParams_url(config.tracksBucketName, beat.beatImage));
+
+                    const beatDTO: BeatReturnDTO = {
+                        id: beat.id, 
+                        jacketImage: imageURL as string,
+                        beatWavFile: beatURL as string,
+                        title: beat.title,
+                        content: beat.introduce as string,
+                        keyword: beat.keyword,
+                        category: beat.category[0] as string,
+                        wavFileLength: beat.duration,
+                        isSelected: beat.isClosed,
+                    };
+
+                    return beatDTO;
+                }));
+
+                return beatList;
+            });
+
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export default findProducerBeatsById;
--- src/global/middlewares/error/errorInstance/profile/index.ts
+++ src/global/middlewares/error/errorInstance/profile/index.ts
@@ -3,5 +3,6 @@ export { UpdateVocalProfileFail } from './UpdateVocalProfileFail';
 export { GetImageFail } from './GetImageFail';
 export { GetProducerInfoFail } from './GetProducerInfoFail';
 export { GetVocalInfoFail } from './GetVocalInfoFail';
+export { GetProducerBeatsFail } from './GetProducerBeatsFail';
 export { InvalidProducer } from './InvalidProducer';
 export { InvalidVocal } from './InvalidVocal';
