e938a644d22c72169569f94aad39749526d4e9a2
dahyunkim <dahyun_ee@naver.com>
2023-02-26 00:03:21 +0900

    [Test] 노드메일러 테스트

--- src/global/config/mailClient.ts
+++ src/global/config/mailClient.ts
@@ -15,3 +15,4 @@ const smtpTransport = nodemailer.createTransport({
 });
 
 export default smtpTransport;
+
--- src/global/modules/sendAuthCodeMail.ts
+++ src/global/modules/sendAuthCodeMail.ts
@@ -3,6 +3,7 @@ import smtpTransport from '../config/mailClient';
 
 const sendAuthCodeMail = async(userEmail: string, authCode: string, image: string) => {
     try {
+        console.log(smtpTransport)
         await smtpTransport.sendMail({
             from: 'admin@track-1.link',
             to: userEmail,
