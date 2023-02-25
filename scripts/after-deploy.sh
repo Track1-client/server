ee9bb78604634aa841115d610bebbacc607b39dd
dahyunkim <dahyun_ee@naver.com>
2023-02-25 23:32:06 +0900

    [Hotfix] .ejs폴더 ci/cd 경로 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,7 +4,7 @@ sudo pm2 kill
 cd $REPOSITORY
 
 sudo rm -rf node_modules
-sudo cp -r src/global/config/ ./dist/global/config/
+sudo cp -r ./src/global/config/template ./dist/global/config/template  #.ejs파일들 dist안으로 넣기 
 sudo yarn install --frozen-lockfile
 sudo npx prisma generate
 sudo pm2 start dist
