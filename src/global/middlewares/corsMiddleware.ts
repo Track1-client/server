acf16d513b3969264f744c512505302d336311e0
dahyunkim <dahyun_ee@naver.com>
2023-02-28 23:52:56 +0900

    [Hotfix] cookie 설정 헤더 추가

--- src/global/middlewares/corsMiddleware.ts
+++ src/global/middlewares/corsMiddleware.ts
@@ -6,10 +6,10 @@ function corsMiddleware(corsOriginList: string[]) {
     
         if (corsOriginList.includes(origin)) {
             res.setHeader('Access-Control-Allow-Origin', origin);
-            res.setHeader('Access-Contol-Allow-Credentials', 'true');
+            res.setHeader('Access-Control-Allow-Credentials', 'true');
         }
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-        res.header(
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+        res.setHeader(
             'Access-Control-Allow-Headers',
             'X-Requested-With, content-type, x-access-token',
         );
