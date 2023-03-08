99b0f68e511c36eb1e1159abae1572b8f92f9524
dahyunkim <dahyun_ee@naver.com>
2023-03-09 06:00:30 +0900

    [Fix] ejs파일 경로 변경

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -5,7 +5,7 @@ cd $REPOSITORY
 
 sudo rm -rf node_modules
 sudo cp -r ./config/logs/error ./dist/config  # 로그파일
-sudo cp -r ./src/global/config/template ./dist/config/template  #.ejs파일들 dist안으로 넣기 
+sudo cp -r ./src/global/config/template ./dist/src/global/config/template  #.ejs파일들 dist안으로 넣기 
 sudo yarn install --frozen-lockfile
 sudo npx prisma generate
 
