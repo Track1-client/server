8d7464a176ddf5b2302170fbba45621402b76c27
dahyunkim <dahyun_ee@naver.com>
2023-03-09 06:33:57 +0900

    [Fix] 파일 경로 변경

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,7 +4,7 @@ sudo pm2 kill
 cd $REPOSITORY
 
 sudo rm -rf node_modules
-sudo cp -r ./config/logs/error ./dist/config  # config의 로그파일
+sudo cp -r ./config/logs/error ./dist/config  # config/logs/error 파일은 dist에 포함 안돼서 따로 추가
 sudo cp -r ./src/global/config/template ./dist/src/global/config  #.ejs파일들 dist안으로 넣기 
 sudo yarn install --frozen-lockfile
 sudo npx prisma generate
