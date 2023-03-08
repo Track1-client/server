02c35976aa0445173c007ef409dcf2139780cd0a
dahyunkim <dahyun_ee@naver.com>
2023-03-09 05:33:50 +0900

    [Fix] package.json 추가

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -8,4 +8,5 @@ sudo cp -r ./config/logs/error ./dist/config  # 로그파일
 sudo cp -r ./src/global/config/template ./dist/config/template  #.ejs파일들 dist안으로 넣기 
 sudo yarn install --frozen-lockfile
 sudo npx prisma generate
+
 sudo pm2 start dist/src
