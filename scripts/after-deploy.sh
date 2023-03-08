58fec488a2a2f1920405e533270a60e37f4470b1
dahyunkim <dahyun_ee@naver.com>
2023-03-09 06:24:15 +0900

    [Fix] after-deploy.sh 경로 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,8 +4,8 @@ sudo pm2 kill
 cd $REPOSITORY
 
 sudo rm -rf node_modules
-sudo cp -r ./config/logs/error ./dist/config  # 로그파일
-sudo cp -r ./src/global/config/template ./dist/src/global/config/template  #.ejs파일들 dist안으로 넣기 
+sudo cp -r ./config/logs/error ./dist/config  # config의 로그파일
+sudo cp -r ./src/global/config/template ./dist/src/global/config  #.ejs파일들 dist안으로 넣기 
 sudo yarn install --frozen-lockfile
 sudo npx prisma generate
 
