51cc06b322b2b6e79cba33f62797349c43e30919
dahyunkim <dahyun_ee@naver.com>
2023-03-13 17:01:38 +0900

    [Fix] after-deploy.sh

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,7 +4,7 @@ sudo pm2 kill
 cd $REPOSITORY
 
 sudo rm -rf node_modules
-sudo cp -r ./config/logs/* ./dist/config  # config/logs/error 파일은 dist에 포함 안돼서 따로 추가
+sudo cp -r ./config/logs/* ./dist/config  # config/logs 안의 파일은 dist에 포함 안돼서 따로 추가 -> config/logs아래의 모든 폴더 복사 
 sudo cp -r ./src/global/config/template ./dist/src/global/config  #.ejs파일들 dist안으로 넣기 
 sudo yarn install --frozen-lockfile
 sudo npx prisma generate
